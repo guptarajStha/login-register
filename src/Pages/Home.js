30b89819e7be65ab9438cdfb59d70c0e26a835e7
guptarajStha <shresthaguptaraj@gmail.com>
2023-01-12 11:15:41 +0545

    home Changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+  const logoutHandler =()=>{
+  
+  }
   return (
-    <div>Home</div>
+    <div className='w-[100vw] h-[100vh] flex flex-col justify-center items-center'>
+      <button onClick={logoutHandler} className='bg-gray-400 p-2 rounded-lg text-white hover:bg-gray-200 '>Log out</button>
+    </div>
   )
 }
 
